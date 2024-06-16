318be350a165b2757a34c38945f5427c0bccc315
IrinaRamanouskaya <irisharomka96@gmail.com>
2024-06-16 20:18:51 +0200

    feat(task-3) start use products service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
