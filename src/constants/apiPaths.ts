cfa05104a59d03e83c9e2b9ca7f2db12f599e6d3
IrinaRamanouskaya <irisharomka96@gmail.com>
2024-06-17 08:17:12 +0200

    feat(task-3): update mock data and apiPaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://r3e5es9vb9.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://acrbd5at83.execute-api.us-east-2.amazonaws.com/prod",
+  order: "https://acrbd5at83.execute-api.us-east-2.amazonaws.com/prod",
+  import: "https://acrbd5at83.execute-api.us-east-2.amazonaws.com/prod",
+  bff: "https://acrbd5at83.execute-api.us-east-2.amazonaws.com/prod",
+  cart: "https://acrbd5at83.execute-api.us-east-2.amazonaws.com/prod",
 };
 
 export default API_PATHS;
